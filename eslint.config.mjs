ef96801af8d5f8bffe3a2ba522e39b4589347f81
Mikelle Wade <mikelle.wade@gadadevelopersacademy.org>
2025-11-16 18:25:49 -0500

    updates eslint configuration file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,46 @@
+import jest from "eslint-plugin-jest";
 import globals from "globals";
-import pluginJs from "@eslint/js";
+import js from "@eslint/js";
 
-/** @type {import('eslint').Linter.Config[]} */
+
 export default [
-  {ignores: ["*.config.*"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  js.configs.recommended,
+  { ignores: ["*.config.*"] },
   {
+    plugins: {
+      jest,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",   
+    },
+
     rules: {
       "max-len": [1, 120, 2, { "ignoreComments": true }],
+      "no-unused-vars": "warn",
       "no-console": "off",
-      "quotes": ["error", "single"],
-      "camelcase": ["error", {"properties": "always"}],
-      "semi": ["warn", "always"],
       "comma-dangle": ["warn", "only-multiline"],
+      "semi": ["warn", "always"],
+      quotes: [2, "single",
+        {
+          allowTemplateLiterals: true,
+          avoidEscape: true,
+        },
+      ],
+      "camelcase": ["error", {"properties": "always"}],
       "dot-notation": "warn",
       "space-before-function-paren": "off",
       "indent": ["warn", 2],
-      "padded-blocks": "warn",
+      "padded-blocks": "off",
       "no-trailing-spaces": "warn",
       "array-bracket-spacing": "warn",
       "no-multi-spaces": ["error", { "ignoreEOLComments": true }],
-      "padded-blocks": ["error", "never"],
       "no-var": "error",
     },
   },
